e2aec17448372d748697ca0d4b8180f505d10f86
clintmadeit <clintonbrirdley@gmail.com>
2024-08-15 15:33:56 +0300

    complete restaurant card details, spacer implementation nad further styling

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -3,6 +3,11 @@ import React from "react";
 import { Card } from "react-native-paper";
 import restImage from "../../../../assets/images/rest_01.jpg";
 import styled from "styled-components/native";
+import { SvgXml } from "react-native-svg";
+
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+import { Spacer } from "./spacer/spacer.component";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.secondary};
@@ -13,26 +18,89 @@ const RestaurantCardCover = styled(Card.Cover)`
   background-color: ${(props) => props.theme.colors.bg.secondary};
 `;
 
-const Title = styled.Text`
-font-family: ${(props) => props.theme.fonts.body};
-  padding: ${(props) => props.theme.space.md};
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Title = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
   color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space.md};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space.sm};
+  padding-bottom: ${(props) => props.theme.space.sm};
+`;
+
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+const Open = styled(SvgXml)`
+  flex-direction: row;
+`;
+
+const Closed = styled.Text`
+  color: ${(props) => props.theme.colors.ui.error};
+`;
+
+const Image = styled.Image`
+  width: 15px;
+  height: 15px;
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [],
     address = "100 some random street",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily = false,
+    isClosedTemporarily = true,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={restImage} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map((_, i) => (
+              <SvgXml key={i} xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Closed>
+                <Text variant="label">CLOSED TEMPORARILY</Text>
+              </Closed>
+            )}
+            <Spacer variant="left.large" />
+            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            <Spacer variant="left.large" />
+            <Image source={{ uri: icon }} />
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
--- src/infrastructure/theme/fonts.js
+++ src/infrastructure/theme/fonts.js
@@ -1,6 +1,6 @@
 export const fonts = {
   body: "Oswald_400Regular",
-  heading: "Lato_700Bold",
+  heading: "Lato_400Regular",
   monospace: "Oswald_400Regular",
 };
 
