216bc44d61d2d8abecea831cd21b5099be56ae7d
clintmadeit <clintonbrirdley@gmail.com>
2024-08-18 17:16:31 +0300

    installation of firebase and initializing the configurations

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import "./gesture-handler";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
+import { initializeApp } from "firebase/app";
 
 import {
   useFonts as useOswald,
@@ -16,6 +17,20 @@ import { RestaurantsContextProvider } from "./src/services/restaurants/restauran
 import { LocationContextProvider } from "./src/services/locations/location.context";
 import { Navigation } from "./src/infrastructure/navigation";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+
+// Init firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBerv3gxQW_sPjsalVIUX2LqO3-0J1DH4A",
+  authDomain: "meals-mingle.firebaseapp.com",
+  projectId: "meals-mingle",
+  storageBucket: "meals-mingle.appspot.com",
+  messagingSenderId: "982330948880",
+  appId: "1:982330948880:web:b43df9f40d1fb0bce0b995",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
